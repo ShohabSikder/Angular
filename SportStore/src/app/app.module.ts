6a355b9ad6f88e5eb533dd6cf5fc299a959dd2d7
ShohabSikder <132428937+ShohabSikder@users.noreply.github.com>
2023-11-27 13:12:14 +0600

    Sport
    Sport

--- SportStore/src/app/app.module.ts
+++ SportStore/src/app/app.module.ts
@@ -1,19 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { StoreModule } from "./store/store.module";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreComponent } from './store/store.component';
+import { provideHttpClient } from '@angular/common/http';
+import { StaticDataSource } from '../model/static.datasource';
+import { ProductRepository } from '../model/product.repository';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //StoreComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule
+    
   ],
   providers: [
-    provideClientHydration()
+    provideHttpClient(),
+    StaticDataSource,
+    ProductRepository
   ],
   bootstrap: [AppComponent]
 })
---
+++ SportStore/src/app/store/store.module.ts
@@ -0,0 +1,13 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { ModelModule } from "../../model/model.module";
+import { FormsModule } from "@angular/forms";
+import { NgModule } from "@angular/core";
+import { StoreComponent } from "./store.component";
+import { CounterDirective } from "./counter.directive";
+
+@NgModule({
+    imports: [ModelModule, BrowserModule, FormsModule],
+    declarations: [StoreComponent,CounterDirective],
+    exports: [StoreComponent]
+   })
+   export class StoreModule { }
