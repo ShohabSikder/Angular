4df46915640ca2a1d409ea31c508f92076950ca1
ShohabSikder <132428937+ShohabSikder@users.noreply.github.com>
2023-11-30 13:22:19 +0600

    add
    add

--- SportStore/src/model/static.datasource.ts
+++ SportStore/src/model/static.datasource.ts
@@ -1,15 +1,16 @@
 import { Injectable } from "@angular/core";
 import { Product } from "./product.model";
 import { Observable, from } from "rxjs";
+import { Order } from "./order.model";
 
 @Injectable()
 export class StaticDataSource {
  private products: Product[] = [
- new Product(1, "Product 1", "Category 1", "Product 1 (Category 1)", 100),
- new Product(2, "Product 2", "Category 1", "Product 2 (Category 1)", 100),
- new Product(3, "Product 3", "Category 1", "Product 3 (Category 1)", 100),
- new Product(4, "Product 4", "Category 1", "Product 4 (Category 1)", 100),
- new Product(5, "Product 5", "Category 1", "Product 5 (Category 1)", 100),
+ new Product(1, "Iphone", "Mobile", "Iphone (Mobile)", 100),
+ new Product(2, "Samsung", "Mobile", "Samsung (Mobile)", 100),
+ new Product(3, "Nothing", "Mobile", "Nothing (Mobile)", 100),
+ new Product(4, "OnePlus", "Mobile", "OnePlus (Mobile)", 100),
+ new Product(5, "Xioami", "Mobile", "Xioami (Mobile)", 100),
  new Product(6, "Product 6", "Category 2", "Product 6 (Category 2)", 100),
  new Product(7, "Product 7", "Category 2", "Product 7 (Category 2)", 100),
  new Product(8, "Product 8", "Category 2", "Product 8 (Category 2)", 100),
@@ -24,4 +25,8 @@ export class StaticDataSource {
  getProducts(): Observable<Product[]> {
  return from([this.products]);
  }
+ saveOrder(order: Order): Observable<Order> {
+    console.log(JSON.stringify(order));
+    return from([order]);
+    }
 }
