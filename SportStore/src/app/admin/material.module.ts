9abbc0fc8c4b2c6b0aa761e063d3214bc003cea2
ShohabSikder <132428937+ShohabSikder@users.noreply.github.com>
2023-12-09 09:18:58 +0600

    add
    add

--- SportStore/src/app/admin/material.module.ts
+++ SportStore/src/app/admin/material.module.ts
@@ -4,8 +4,13 @@ import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from "@angular/material/button";
+import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatPseudoCheckboxModule } from "@angular/material/core";
+
 const features: any[] = [MatToolbarModule, MatSidenavModule, MatIconModule,
-    MatDividerModule, MatButtonModule];
+    MatDividerModule, MatButtonModule,MatTableModule,MatPaginatorModule,MatFormFieldModule, MatIconModule,MatPseudoCheckboxModule];
 @NgModule({
     imports: [features],
     exports: [features]
